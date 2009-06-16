cb7edcc7ecdb8cc39e96663b3d1834ae28415bf5
stefan7th (Stefan Theussl) <stefan.theussl@R-project.org>
2009-06-16 21:21:32 +0000

    improved install script
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/aix@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- install_scripts/install_R_AIX.sh
+++ install_scripts/install_R_AIX.sh
@@ -1,10 +1,78 @@
+#!/bin/bash
+## Script for building R on AIX
+## Author: Theussl
+## Last change: 2009-06-16
 
-echo "Start building R for AIX ..."
+## First define configure arguments for different compilers
+configure_args=
+
+## standard GNU compilers
+configure_args_gnu="\
+  CC=\"gcc -maix64 -pthread\" \
+  CXX=\"g++ -maix64 -pthread\" \
+  FC=\"gfortran -maix64 -pthread\" \
+  F77=\"gfortran -maix64 -pthread\" \
+  CFLAGS=\"-O2 -g -mcpu=power6\" \
+  FFLAGS=\"-O2 -g -mcpu=power6\" \
+  FCFLAGS=\"-O2 -g -mcpu=power6\"\
+  MAKE=/opt/freeware/bin/make"
+
+## IBM compilers and ESSL
+configure_args_ibm="\
+  --with-blas=\"-lessl -lblas\" \
+  CC=xlc \
+  CXX=xlc++ \
+  FC=xlf \
+  F77=xlf \
+  CFLAGS=\"-qarch=auto -qcache=auto -qtune=auto -O3 -qstrict\" \
+  FFLAGS=\"-qarch=auto -qcache=auto -qtune=auto -O3 -qstrict\" \
+  FCFLAGS=\"-qarch=auto -qcache=auto -qtune=auto -O3 -qstrict\" \
+  CXXFLAGS=\"-qarch=auto -qcache=auto -qtune=auto -O3 -qstrict\" \
+  MAKE=/opt/freeware/bin/make"
+
+while test -n "${1}"; do
+    case "${1}" in
+	--gnu)
+	    configure_args=${configure_args_gnu}
+	    COMPILER="gcc"
+	    ;;
+	--ibm)
+	    configure_args=${configure_args_ibm}
+	    COMPILER="xlc"
+	    ;;
+	*)
+	    configure_args="${configure_args} ${1}"
+	    ;;
+    esac
+    shift
+done
+
+if test -z "${COMPILER}"; then
+    echo "No compilers specified"
+    exit 1
+fi
+
+## Setup directories
 
 BUILD_DIR=/home/theussl/src/Rbuild
-SVN_SRC_DIR=/home/theussl/svn/R-2-8-branch
-AIX_PATCH=/home/theussl/AIX_scripts/aix_essl_R_m4.patch
-R_CONFIGURE=/home/theussl/AIX_scripts/Rconf_xlc_essl
+SVN_SRC_DIR=/home/theussl/svn/R-2-9-branch
+PATCH_AIX_R_ESSL=/home/theussl/AIX_scripts/aix_essl_R_m4.patch
+PATCH_AIX_mgcv_gdi=/home/theussl/AIX_scripts/aix_mgcv_gdi_c.patch
+
+DATE=`date +%y-%m-%d`
+DIR_PREFIX=/usr/local/share/R
+FLAVOR=R-patched
+
+TARGET_DIR=${COMPILER}-build-${DATE}
+INSTALL_DIR=${DIR_PREFIX}/${FLAVOR}/${TARGET_DIR}
+
+## Everything set up -> build R
+
+echo "$DATE: Start building R for AIX ..."
+echo "#############################################################"
+
+echo "Step 1: Initialization"
+echo "#############################################################"
 
 if [[ -d $BUILD_DIR ]] ; then
     echo "Remove old build directory ..."
@@ -19,21 +87,57 @@ cp -rp $SVN_SRC_DIR $BUILD_DIR
 
 cd $BUILD_DIR
 
-echo "Patch R.m4 to allow ESSL on AIX ..."
-cat $AIX_PATCH | patch ./m4/R.m4
+echo "Step 2: Apply R/AIX patches and sync recommended packages"
+echo "#############################################################"
 
-echo "Run aclocal ..."
-aclocal -I ./m4
+if [[ $COMPILERS = "xlc" ]] ; then
+    echo "Patch R.m4 to allow ESSL on AIX ..."
+    cat $PATCH_AIX_R_ESSL | patch ./m4/R.m4
 
-echo "Run autoconf ..."
-autoconf
+    echo "Run aclocal ..."
+    aclocal -I ./m4
+
+    echo "Run autoconf ..."
+    autoconf
+fi
 
 echo "Sync recommended packages ..."
 ./tools/rsync-recommended 
 
-echo "Configure R ..."
-$R_CONFIGURE 
+echo "Patch gdi.c in mgcv ..."
+cd ./src/library/Recommended
+tar xzf mgcv_*.tar.gz
+cat $PATCH_AIX_mgcv_gdi | patch ./mgcv/src/gdi.c
+rm mgcv_*.tar.gz
+R CMD build mgcv
+rm -rf ./mgcv
+cd $BUILD_DIR
 
+
+echo "Step 3: Configure R"
+echo "#############################################################"
+
+(eval ./configure --prefix=${INSTALL_DIR} ${configure_args})
+
+echo "Step 4: Build R"
+echo "#############################################################"
+
+MAKE="make -j8" make
+
+echo "Step 5: Install R"
+echo "#############################################################"
+
+echo "If you don't have sudo privileges go to $BUILD_DIR and type"
+echo "'make install' as root to install R"
+
+sudo make install
+
+echo "Step 5: Finalization"
+echo "#############################################################"
+
+cd ${DIR_PREFIX}/${FLAVOR}
+sudo rm -f current
+sudo ln -s ${TARGET_DIR} current
+
+echo "#############################################################"
 echo "Done."
-
-
